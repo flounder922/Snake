f24b581d4cfe3025a4e258f755d5d72c0ca21e38
flounder922 <flounder922>
2020-02-29 13:18:38 -0800

    Added functionality to Point class

--- app/src/main/java/com/example/snake/Point.java
+++ app/src/main/java/com/example/snake/Point.java
@@ -1,4 +1,33 @@
 package com.example.snake;
 
 public class Point {
+    // The location variables on a 2D screen.
+    public int x;
+    public int y;
+
+    // Base constructor for the point class
+    Point(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // Overloaded constructor that accepts the coordinates upon creation.
+    Point( int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Check to see if coordinates are the same.
+    public final boolean equals(int x, int y) {
+        if (this.x == x && this.y == y)
+            return true;
+        else
+            return false;
+    }
+
+    // Change the position of the point
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
